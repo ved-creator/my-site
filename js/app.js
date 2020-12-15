9a86d2b4bc12debd8d094236812a13ed9a9392db
Ved <ved.43d@gmail.com>
2020-12-15 19:11:33 +0200

    Верстка меню

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	var menu_button = document.querySelector('.main-nav__toggle');
+	var menu = document.querySelector('.main-nav');
+
+	menu_button.addEventListener('click', function(e){
+		e.preventDefault();
+		if(menu.classList.contains('main-nav--active')){
+			menu.classList.remove('main-nav--active');
+			menu_button.classList.remove('toggle--active');
+		}else{
+			menu.classList.add('main-nav--active');
+			menu_button.classList.add('toggle--active');
+		}
+	});
 
 });
